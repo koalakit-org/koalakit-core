37d6aec8fdb806e186b71224e9932e8b79cadc6b
m.shreem <mshrem@moj.gov.sa>
2022-02-14 11:11:00 +0300

    add specifications and add store basic functions

---
+++ src/core/KoalaKit.Abstractions/Persistence/Specifications/ISpecification.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq.Expressions;
+
+namespace KoalaKit.Persistence.Specifications
+{
+    public interface ISpecification<T>
+    {
+        bool IsSatisfiedBy(T entity);
+        Expression<Func<T, bool>> ToExpression();
+    }
+}
