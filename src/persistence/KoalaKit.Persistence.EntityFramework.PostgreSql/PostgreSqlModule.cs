bed5a5fc0a4d58d655423c612e39810a75f34bae
m.shreem <mshrem@moj.gov.sa>
2022-02-14 14:56:31 +0300

    add Postgree Module

---
+++ src/persistence/KoalaKit.Persistence.EntityFramework.PostgreSql/PostgreSqlModule.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace KoalaKit.Persistence.EFCore.PostgreSql
+{
+    internal class PostgreSqlModule : EfCoreModuleBase
+    {
+        protected override string ProviderName => "PostgreSql";
+
+        protected override void Configure(DbContextOptionsBuilder builder, string connectionString)
+            => builder.UsePostgreSql(connectionString);
+    }
+}
