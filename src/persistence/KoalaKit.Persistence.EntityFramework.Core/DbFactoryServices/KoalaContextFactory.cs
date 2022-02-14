23b1d51784e87bb31c3ed6141b20c06a0dbabd5f
m.shreem <mshrem@moj.gov.sa>
2022-02-14 13:00:37 +0300

    register ef core add ef core tasks and migration

---
+++ src/persistence/KoalaKit.Persistence.EntityFramework.Core/DbFactoryServices/KoalaContextFactory.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace KoalaKit.Persistence.EFCore.DbFactoryServices
+{
+    public class KoalaContextFactory<TKoalaContext> : IKoalaContextFactory where TKoalaContext : KoalaDbContext
+    {
+        private readonly IDbContextFactory<TKoalaContext> contextFactory;
+        public KoalaContextFactory(IDbContextFactory<TKoalaContext> contextFactory) => this.contextFactory = contextFactory;
+        public KoalaDbContext CreateDbContext() => contextFactory.CreateDbContext();
+    }
+}
---
+++ src/persistence/KoalaKit.Persistence.EntityFramework.Core/EntityFrameworkCoreModuleBase.cs
@@ -0,0 +1,34 @@
+﻿using KoalaKit.Modules;
+using KoalaKit.Options;
+using KoalaKit.Persistence.EFCore.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace KoalaKit.Persistence.EFCore
+{
+    public abstract class EntityFrameworkCoreModuleBase : KoalaModuleBase
+    {
+        protected abstract string ProviderName { get; }
+        public override void ConfigureKoala(KoalaOptionsBuilder koala)
+        {
+            var section = koala.Configuration.GetSection($"Koala:DefaultPersistence");
+            var connectionStringName = section.GetValue<string>("ConnectionStringIdentifier");
+            var connectionString = section.GetValue<string>("ConnectionString");
+
+            if (string.IsNullOrWhiteSpace(connectionString))
+            {
+                if (string.IsNullOrWhiteSpace(connectionStringName))
+                    connectionStringName = ProviderName;
+
+                connectionString = koala.Configuration.GetConnectionString(connectionStringName);
+            }
+
+            if (string.IsNullOrWhiteSpace(connectionString))
+                connectionString = GetDefaultConnectionString();
+
+            koala.UseEfPersistence(options => Configure(options, connectionString));
+        }
+        protected abstract void Configure(DbContextOptionsBuilder builder, string connectionString);
+        protected virtual string GetDefaultConnectionString() => throw new Exception($"No connection string specified for the {ProviderName} provider");
+    }
+}
