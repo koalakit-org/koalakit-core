655f834e3264bee3c20ab50bef9cdbe8d69a4692
m.shreem <mshrem@moj.gov.sa>
2022-02-16 10:40:38 +0300

    rabbit mq queuing

---
+++ messaging/queuing/KoalaKit.Queuing.RabbitMq/QueuingMessageRabbitMqPublisher.cs
@@ -0,0 +1,28 @@
+﻿using KoalaKit.Messaging;
+using KoalaKit.Queuing.RabbitMq.Extensions;
+using KoalaKit.Serializations;
+using RabbitMQ.Client;
+
+namespace KoalaKit.Queuing.RabbitMq
+{
+    public class QueuingMessageRabbitMqPublisher<TMessage> : IMessageQueuingPublisher<TMessage> where TMessage : IQueuingMessage, new()
+    {
+        private readonly IMessageQueueFactory<TMessage> queueFactory;
+        private readonly ISerializer<TMessage> serializer;
+
+        public QueuingMessageRabbitMqPublisher(
+            IMessageQueueFactory<TMessage> queueFactory,
+            ISerializer<TMessage> serializer)
+        {
+            this.queueFactory = queueFactory;
+            this.serializer = serializer;
+        }
+
+        public void Publish(TMessage message)
+        {
+            var queue = queueFactory.Create(message);
+            var model = RabbitMqExtensions.InitializeRabbitMqModel(queue, false);
+            model.BasicPublish(queue.Connection.Exchange, queue.Route, model.CreateBasicProperties(), serializer.Serialize(message));
+        }
+    }
+}
