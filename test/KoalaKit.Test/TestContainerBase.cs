fd3ce81559e54b99f31e9089c851036f06bc648c
m.shreem <mshrem@moj.gov.sa>
2022-02-19 02:50:42 +0300

    add unit testing base module

---
+++ test/KoalaKit.Test/TestContainerBase.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace KoalaKit.Test
+{
+    public abstract class TestContainerBase
+    {
+        protected TestContainerBase()
+        {
+            Configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings-test.json")
+                .Build();
+            var serviceCollection = new ServiceCollection();
+
+            ConfigureServices(serviceCollection);
+            ServiceProvider = serviceCollection.BuildServiceProvider();
+
+            ResolveServices();
+        }
+
+        protected IServiceProvider ServiceProvider { get; private set; }
+        protected IConfiguration Configuration { get; private set; }
+
+        protected virtual void ConfigureServices(IServiceCollection services) { }
+        protected virtual void ResolveServices() { }
+    }
+}
