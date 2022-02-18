479afe6b5eae627e4fecb40e57271cb9ed80d213
m.shreem <mshrem@moj.gov.sa>
2022-02-19 02:33:40 +0300

    pub/sub handler

--- messaging/Bus/KoalaKit.Bus.Redis/RedisSubChannel.cs
+++ messaging/Bus/KoalaKit.Bus.Redis/RedisSubChannel.cs
@@ -1,39 +1,41 @@
-﻿using KoalaKit.Messaging.Bus;
+﻿using KoalaKit.Messaging;
+using KoalaKit.Messaging.Bus;
 using KoalaKit.Serializations;
+using Microsoft.Extensions.DependencyInjection;
 using StackExchange.Redis;
 
 namespace KoalaKit.Bus.Redis
 {
-    public class RedisSubChannel<TMessage> : ISubChannel<TMessage> where TMessage : class, IBusMessage
+    public class RedisSubChannel<TMessage> : ISubChannel<TMessage>
+        where TMessage : IBusMessage, new()
     {
         private readonly IConnectionMultiplexer multiplexer;
         private readonly IBusMessageDefinitionFactory<TMessage> definitionFactory;
         private readonly ISerializer<TMessage> serializer;
+        private readonly IServiceProvider serviceProvider;
 
         public RedisSubChannel(
             IConnectionMultiplexer multiplexer,
             IBusMessageDefinitionFactory<TMessage> definitionFactory,
-            ISerializer<TMessage> serializer)
+            ISerializer<TMessage> serializer,
+            IServiceProvider serviceProvider)
         {
             this.multiplexer = multiplexer;
             this.definitionFactory = definitionFactory;
             this.serializer = serializer;
+            this.serviceProvider = serviceProvider;
         }
 
 
         public async Task SubscribeAsync()
         {
-            var messageBus = definitionFactory.Create();
-            await multiplexer.GetSubscriber().SubscribeAsync(messageBus.Channel, (channel, redisValue) =>
-            {
-                //implement the handlers
-            });
+            await multiplexer.GetSubscriber().SubscribeAsync(definitionFactory.Create().Channel, Handler);
         }
 
         public async Task SubscribeAsync(Action<TMessage> handler)
         {
             var messageBus = definitionFactory.Create();
-            await multiplexer.GetSubscriber().SubscribeAsync(messageBus.Channel, (channel, redisValue) =>
+            await multiplexer.GetSubscriber().SubscribeAsync(messageBus.Channel, (_, redisValue) =>
             {
                 var message = serializer.Deserialize(redisValue);
                 if(message == null) return;
@@ -43,5 +45,16 @@ namespace KoalaKit.Bus.Redis
         }
 
         public async Task UnsubscribeAsync() => await multiplexer.GetSubscriber().UnsubscribeAsync(definitionFactory.Create().Channel);
+
+
+        private async void Handler(RedisChannel channel, RedisValue redisValue)
+        {
+            var message = serializer.Deserialize(redisValue);
+            if (message == null) return;
+
+            var handler = serviceProvider.CreateScope().ServiceProvider.GetService<IMessagingHandler<TMessage>>();
+            if (handler != null)
+                await handler.HandleAsync(message);
+        }
     }
 }
