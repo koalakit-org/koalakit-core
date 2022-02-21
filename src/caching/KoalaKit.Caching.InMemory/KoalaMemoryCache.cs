fa4977295bb20650c23defa77e83547ae2f9bf57
m.shreem <mshrem@moj.gov.sa>
2022-02-22 02:11:13 +0300

    make not implement

--- src/caching/KoalaKit.Caching.InMemory/KoalaMemoryCache.cs
+++ src/caching/KoalaKit.Caching.InMemory/KoalaMemoryCache.cs
@@ -2,17 +2,24 @@
 
 namespace KoalaKit.Caching.InMemory
 {
+    //Inprogress. assumed as not implemented.
     internal class KoalaMemoryCache<T> : ICache<T>
     {
-        private readonly Dictionary<string, T> cache = new();
+        private readonly IMemoryCache cache;
+
+        public KoalaMemoryCache(IMemoryCache cache)
+        {
+            this.cache = cache;
+        }
+
         public T Get(string key)
         {
-            return cache.ContainsKey(key) ? cache[key] : default(T);
+            return cache.TryGetValue(key, out var value) ? (T) value : default(T);
         }
 
         public void Set(string key, T value)
         {
-            cache[key] = value;
+            cache.Set(key, value);
         }
     }
 }
