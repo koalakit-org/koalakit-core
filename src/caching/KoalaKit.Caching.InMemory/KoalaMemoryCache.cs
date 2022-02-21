ef4e6d466f1c2993b0897bc503b4d4897ccbb207
m.shreem <mshrem@moj.gov.sa>
2022-02-22 00:13:44 +0300

    simply the memory cache

--- src/caching/KoalaKit.Caching.InMemory/KoalaMemoryCache.cs
+++ src/caching/KoalaKit.Caching.InMemory/KoalaMemoryCache.cs
@@ -4,57 +4,15 @@ namespace KoalaKit.Caching.InMemory
 {
     internal class KoalaMemoryCache<T> : ICache<T>
     {
-
-        public KoalaMemoryCache(IMemoryCache memoryCache)
-        {
-            this.memoryCache = memoryCache;
-        }
-
-
+        private readonly Dictionary<string, T> cache = new();
         public T Get(string key)
         {
-            if(memoryCache.TryGetValue(key, out var v))
-                return (T)v;
-            
-            string timestamp = memoryCache.GetOrCreate(key, entry =>
-            {
-                entry.AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(5);
-                return DateTime.Now.ToString();
-            });
-
-            //memoryCache.GetOrCreateAsync()
-
+            return cache.ContainsKey(key) ? cache[key] : default(T);
         }
 
         public void Set(string key, T value)
         {
-            var entry = memoryCache.CreateEntry(key);
-            entry.Value = value;
-
-            entry.
-        }
-    }
-
-    public class KoalaMemoryCache : IMemoryCache
-    {
-        public void Dispose()
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool TryGetValue(object key, out object value)
-        {
-            throw new NotImplementedException();
-        }
-
-        public ICacheEntry CreateEntry(object key)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Remove(object key)
-        {
-            throw new NotImplementedException();
+            cache[key] = value;
         }
     }
 }
