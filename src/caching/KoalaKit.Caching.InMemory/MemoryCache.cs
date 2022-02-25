93143cd6b3eec91bd9f1fc8f2e5e9b89dc98f804
m.shreem <mshrem@moj.gov.sa>
2022-02-25 13:23:49 +0300

    memory caching

---
+++ src/caching/KoalaKit.Caching.InMemory/MemoryCache.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace KoalaKit.Caching.InMemory
+{
+    public class MemoryCache : ICache
+    {
+        private readonly IMemoryCache memoryCache;
+        public async Task SetAsync<T>(string key, T data)
+        {
+            memoryCache.Set<T>(key, data);
+        }
+
+        public async Task<T> GetAsync<T>(string key)
+        {
+            return memoryCache.Get<T>(key);
+        }
+    }
+}
---
+++ src/caching/KoalaKit.Caching.InMemory/MemoryCacheProvider.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace KoalaKit.Caching.InMemory
+{
+    public class MemoryCacheProvider : ICacheProvider
+    {
+        public ICache CreateCache()
+        {
+            return new MemoryCache();
+        }
+    }
+}
