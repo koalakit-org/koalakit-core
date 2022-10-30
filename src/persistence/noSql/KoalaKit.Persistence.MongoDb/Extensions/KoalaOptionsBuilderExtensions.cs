b368506cf657d9f4497875af47038abb965356d0
shrembo <mshreem00@gmail.com>
2022-10-31 00:56:38 +0300

    persistance

---
+++ src/persistence/noSql/KoalaKit.Persistence.MongoDb/Extensions/KoalaOptionsBuilderExtensions.cs
@@ -0,0 +1,37 @@
+﻿using KoalaKit.Options;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace KoalaKit.Persistence.MongoDb.Extensions
+{
+    public static class KoalaOptionsBuilderExtensions
+    {
+        public static KoalaOptionsBuilder UseMongoDbPersistence(this KoalaOptionsBuilder koala, Action<KoalaMongoDbOptions> configureOptions)
+        {
+            return UseMongoDbPersistence<KoalaMongoDbContext>(koala, configureOptions);
+        }
+
+
+
+    public static KoalaOptionsBuilder UseMongoDbPersistence<TDbContext>(
+        this KoalaOptionsBuilder koala,
+        Action<KoalaMongoDbOptions> configureOptions)
+            where TDbContext : KoalaMongoDbContext
+        {
+            koala.Services.Configure(configureOptions);
+
+            return koala;
+        }
+    }
+
+
+
+    public class KoalaMongoDbOptions
+    {
+        public string ConnectionString { get; set; }
+    }
+
+    public class KoalaMongoDbContext
+    {
+
+    }
+}
