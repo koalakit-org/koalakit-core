f587ed517c33922c7b0023a07a5a38c3db0fc4b1
shrembo <mshreem00@gmail.com>
2022-11-15 15:23:57 +0300

    redis

---
+++ src/caching/KoalaKit.Caching.Redis/KoalaRedisCacheModule.cs
@@ -0,0 +1,24 @@
+﻿using KoalaKit.Caching;
+using KoalaKit.Modules;
+using KoalaKit.Options;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Koalakit.Caching.Redis
+{
+    public class KoalaRedisCacheModule : KoalaModuleBase
+    {
+        public override void ConfigureKoala(KoalaOptionsBuilder koala)
+        {
+            koala.AddKoalaCachingCore();
+            koala.Services.AddScoped<KoalaRedisCache>();
+            koala.Services.AddScoped(typeof(ICacheProvider<>), typeof(KoalaRedisCacheProvider<>));
+            ///TODO: enhance the configuration.
+            koala.Services.AddStackExchangeRedisCache(options => {
+                options.Configuration = koala.Configuration.GetConnectionString("");
+                options.InstanceName = "";
+            });
+            base.ConfigureKoala(koala);
+        }
+    }
+}
