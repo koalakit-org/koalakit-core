3637d14c2730f030fe09323d787e6547054b6f79
shrembo <mshreem00@gmail.com>
2022-11-15 15:57:14 +0300

    Sql Server

---
+++ src/persistence/efCore/KoalaKit.Persistence.EntityFramework.SqlServer/SqlServerKoalaDbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace KoalaKit.Persistence.EFCore.SqlServer
+{
+    public class SqlServerKoalaDbContextFactory : IDesignTimeDbContextFactory<KoalaDbContext>
+    {
+        public KoalaDbContext CreateDbContext(string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<KoalaDbContext>();
+            var connectionString = args.Any() ? args[0] : throw new InvalidOperationException("");
+            var migrationsAssemblyName = args.Any() ? args[1] : throw new InvalidOperationException("");
+            builder.ConfigureSqlServer(connectionString, migrationsAssemblyName);
+            return new KoalaDbContext(builder.Options);
+        }
+    }
+}
---
+++ src/persistence/efCore/KoalaKit.Persistence.EntityFramework.SqlServer/SqlServerModule.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace KoalaKit.Persistence.EFCore.SqlServer
+{
+    public class SqlServerModule : EfCoreModuleBase
+    {
+        protected override string ProviderName => "SqlServer";
+
+        protected override void Configure(DbContextOptionsBuilder builder, string connectionString, string migrationsAssemblyName)
+            => builder.ConfigureSqlServer(connectionString, migrationsAssemblyName);
+    }
+}
