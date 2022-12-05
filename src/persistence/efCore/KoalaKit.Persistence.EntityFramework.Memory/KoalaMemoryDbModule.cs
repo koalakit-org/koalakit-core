ece3c4dea54bb34880b94d2c3cee9d2394f9c788
shrembo <mshreem00@gmail.com>
2022-12-05 12:37:06 +0300

    memory module

---
+++ src/persistence/efCore/KoalaKit.Persistence.EntityFramework.Memory/KoalaMemoryDbModule.cs
@@ -0,0 +1,12 @@
+﻿using KoalaKit.Persistence.EFCore;
+
+namespace KoalaKit.Persistence.EntityFramework.Memory
+{
+    public class KoalaMemoryDbModule : EfCoreModuleBase
+    {
+        protected override string ProviderName => "SqlServer";
+
+        protected override void Configure(DbContextOptionsBuilder builder, string connectionString, string migrationsAssemblyName)
+            => builder.ConfigureSqlServer(connectionString, migrationsAssemblyName);
+    }
+}
