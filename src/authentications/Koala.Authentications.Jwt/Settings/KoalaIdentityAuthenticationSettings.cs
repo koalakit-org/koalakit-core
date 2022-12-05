19edae1af66b2d5a79cbb53f3f36b8135f4322d0
shrembo <mshreem00@gmail.com>
2022-12-05 23:52:32 +0300

    fix authentication registration

--- src/authentications/Koala.Authentications.Jwt/Settings/KoalaIdentityAuthenticationSettings.cs
+++ src/authentications/Koala.Authentications.Jwt/Settings/KoalaIdentityAuthenticationSettings.cs
@@ -1,6 +1,6 @@
 ﻿namespace Koala.Authentications.Jwt
 {
-    internal class KoalaIdentityAuthenticationSettings : IKoalaIdentityAuthenticationSettings
+    public class KoalaIdentityAuthenticationSettings : IKoalaIdentityAuthenticationSettings
     {
         public string Secret { get; set; } = string.Empty;
         public string Issuer { get; set; } = string.Empty;
