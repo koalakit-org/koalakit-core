d3633ad84f1e12a1b344936d0666202383849b6c
shrembo <mshreem00@gmail.com>
2022-12-12 01:14:40 +0300

    Add new value models

---
+++ src/cosmetics/KoalaKit.Cosmetics/Models/FullName.cs
@@ -0,0 +1,21 @@
+﻿namespace KoalaKit.Cosmetics
+{
+    [Serializable]
+    public struct FullName
+    {
+        ///TODO: Replace with serialize
+        public FullName(string information)
+        {
+            FirstName = information.Split(":")[0];
+            MiddleName = information.Split(":")[1];
+            LastName = information.Split(":")[2];
+        }
+
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+
+
+        public override string ToString() => $"{FirstName}:{MiddleName}:{LastName}";
+    }
+}
---
+++ src/cosmetics/KoalaKit.Cosmetics/Models/JobInformation.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace KoalaKit.Cosmetics
+{
+    [Serializable]
+    public struct JobInformation : IEquatable<JobInformation>
+    {
+        public JobInformation(string information)
+        {
+            Number = information.Split(":")[0];
+            Title= information.Split(":")[1];
+        }
+
+        public string Number { get; set; }
+        public string Title { get; set; }
+
+        public override string ToString() => $"{Number}:{Title}";
+        public int GetHashCode([DisallowNull] JobInformation obj)
+        {
+            return base.GetHashCode();
+        }
+
+        public bool Equals(JobInformation other)
+        {
+            return Number.Equals(other.Number);
+        }
+    }
+}
